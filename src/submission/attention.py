265f38b938b7212c2d7a59df98f88678d282588c
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2022-12-11 23:46:50 +0100

    upd

--- src/submission/attention.py
+++ src/submission/attention.py
@@ -96,6 +96,28 @@ class SynthesizerAttention(nn.Module):
         ###       How do these map to the matrices in the handout?
 
         ### START CODE HERE
+        """
+        B -> batch_size
+        nh -> number of attention heads
+        hs -> dimensionality per attention head
+        C -> total dimension of embeddings
+        T -> sequence length (block_size)
+        """
+        B, T, C = x.size()  # batch_size, sequence_length (block_size), total_dimension
+
+        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        att = F.relu(self.w1(x)).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        att = att @ self.w2[None, None, ..., :T] + self.b2[..., :T]  # (B, nh, T, hs) -> (B, nh, T, T-1)
+        att = att.masked_fill(self.mask[:, :, :T, :T] == 0, float('-inf'))  # Replace masked entries with -inf.
+        att = F.softmax(att, dim=-1)
+        att = self.attn_drop(att)  # Apply dropout.
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+        # contiguous() creates a new copy of the tensor that is same as if it has been created from scratch.
+
+        # output projection
+        y = self.resid_drop(self.proj(y))
+        return y
         ### END CODE HERE
 
-        raise NotImplementedError
+        #raise NotImplementedError
